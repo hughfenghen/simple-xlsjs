971684a2c89c9040e9889a57ffd95ed0ee3da328
hughfenghen <hughfenghen@gmail.com>
2019-05-18 15:28:55 +0800

    example 及 jest配置

---
+++ example/example.ts
@@ -0,0 +1,28 @@
+import { loadWbFromFile, selectSheet, sheet2JSON, selectColumn, selectCell, allRowValues } from '../index';
+
+interface HTMLInputEvent extends Event {
+  target: HTMLInputElement & EventTarget;
+}
+
+;(() => {
+  document
+    .getElementById('file')
+    .addEventListener('change', async (evt?: HTMLInputEvent) => {
+      const file = evt.target.files[0]
+      const wb = await loadWbFromFile(file)
+      const sheet = selectSheet(0, wb)
+
+      const [
+        cellsEl, rowValuesEl, columnEl, jsonEl
+      ] = ['.cells', '.row-values', '.column', '.json']
+          .map((s) => document.querySelector(s))
+      
+      cellsEl.innerHTML = JSON.stringify(selectCell(['A1', 'B2', 'C2'], sheet))
+      
+      rowValuesEl.innerHTML = JSON.stringify(allRowValues(sheet)[2])
+
+      columnEl.innerHTML = JSON.stringify(selectColumn({ A: 'type', B: 'code'}, sheet))
+
+      jsonEl.innerHTML = JSON.stringify(sheet2JSON(sheet))
+    })
+})()
